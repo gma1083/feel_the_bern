655da04bf8c7a34afd743bd78d02c2ada8380c62
Gregory Arnheiter <garnheit@ucsc.edu>
2016-01-31 04:33:09 -0800

    Beginning Formatting and Banner

--- client/views/display_players.js
+++ client/views/display_players.js
@@ -1,4 +1,22 @@
 if (Meteor.isClient){
+
+	Template.display_players.events({
+		'click .reactive-table tbody tr': function (event) {
+	//	console.log(this._id);
+		var currUser = Meteor.user();
+		var currTeam = teams.findOne({owner: currUser.emails[0].address});
+		var currPlayers = currTeam.my_team;
+		//I hate life
+		//this is where we need some fancy conditional to tell us if something is on my god damn team.
+		currPlayers = currPlayers.push(this._id);
+		teams.update(this._id, {
+			$set : {my_team : currPlayers}
+		});
+		console.log(currPlayers);
+
+		}
+	});
+
 	Template.display_players.helpers({
 		settings: function () {
 			return {
@@ -12,10 +30,10 @@ if (Meteor.isClient){
 					{ key: 'polls', label: 'Polls' },
 					{ key: 'funding', label: 'Funding' },
 					{ key: 'bonus', label: 'Bonus' },
-					{ key: 'cost', label: 'Cost' },
-					{label: 'button'}
+					{ key: 'cost', label: 'Cost' }
 				]
 			};
 		}
 	});
 }
+
