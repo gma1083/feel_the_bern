f6cd331fb05470b0095c8850dc4a38984a549c99
Nick Dubelman <Nick@Nicks-MacBook-Pro.local>
2016-02-06 00:24:48 -0800

    major project restructuring, modal working, no more semicolons, lots of stuff

--- client/views/teams/createTeam.js
+++ client/views/teams/createTeam.js
@@ -1,11 +1,11 @@
 // Creates a team
 Template.createTeam.events({
-	'submit form .createTeam' : function (event, template){
+	'submit form' : function (event, template){
   		event.preventDefault(); // we decide the rules
-  		var name = event.target.teamName.value;
+  		var name = event.target.teamName.value
 		Meteor.call('createTeam', name, function(error){
-			if(error) return alert(error.reason);
+			if(error) return alert(error.reason)
 		});
-  		template.find("form").reset();
+  		template.find("form").reset()
 	}
 });
--- client/views/users/register.js
+++ client/views/users/register.js
@@ -1,7 +1,7 @@
 Template.register.events({
     'submit form': function(event){
-        event.preventDefault();
-        console.log("working");
+        event.preventDefault()
+        console.log("working")
         var user = {
         	email: event.target.registerEmail.value,
         	password: event.target.registerPassword.value 
@@ -9,6 +9,13 @@ Template.register.events({
         Accounts.createUser({
             email: user.email,
             password: user.password
+        }, function(error){
+            if(error){
+                alert("all your base")
+            }
+            else{
+                Modal.hide()
+            }
         });
     }
 });
